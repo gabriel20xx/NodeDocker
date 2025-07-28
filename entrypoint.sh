d4bf45a8e2a426450b3f2f7e37e53dc891fd74ac
Gabriel <gabrielfranz31@gmail.com>
2025-07-28 11:00:05 +0200

    Add git branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,13 @@ chown -R 99:100 /app
 chmod -R 777 /app
 
 # Clone repo
-echo "Cloning $GIT_REPO into /app..."
-git clone "$GIT_REPO" /app
+if [ -n "$GIT_BRANCH" ]; then
+  echo "Cloning branch $GIT_BRANCH from $GIT_REPO into /app..."
+  git clone --branch "$GIT_BRANCH" "$GIT_REPO" /app
+else
+  echo "Cloning default branch from $GIT_REPO into /app..."
+  git clone "$GIT_REPO" /app
+fi
 
 # Fix permissions again after clone (in case git resets them)
 chown -R 99:100 /app
